057763b27b05c36ae4e219f032d5b95cebffa4ac
razyfm <mrazyfadhli@gmail.com>
2023-06-01 23:11:32 +0700

    website kopi inspirasi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// KLik di luar sidebar untuk menghilangkan nnav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
